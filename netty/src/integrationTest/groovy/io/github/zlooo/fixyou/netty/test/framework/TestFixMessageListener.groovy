2d9ed0028ac71c3b914864956320e54e6c2ef109
Zlooo <zlooo27@wp.pl>
2020-05-22 17:12:34 +0200

    trying to stabilize tests  - I think there was a race condition here which should be eliminated by change of this data structure

--- netty/src/integrationTest/groovy/io/github/zlooo/fixyou/netty/test/framework/TestFixMessageListener.groovy
+++ netty/src/integrationTest/groovy/io/github/zlooo/fixyou/netty/test/framework/TestFixMessageListener.groovy
@@ -7,10 +7,13 @@ import io.github.zlooo.fixyou.session.SessionID
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.BlockingQueue
+
 class TestFixMessageListener implements FixMessageListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TestFixMessageListener)
-    List<FixMessage> messagesReceived = new ArrayList<>()
+    BlockingQueue<FixMessage> messagesReceived = new ArrayBlockingQueue<>(10)
 
     @Override
     void onFixMessage(SessionID sessionID, FixMessage fixMessage) {
